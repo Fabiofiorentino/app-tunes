09bccc2c24b41e015b7b7dbf6ce9e99a9259045b
Fabio Vilas <fabiovilaas@gmail.com>
2023-04-05 16:06:16 -0300

    instala styled-components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
